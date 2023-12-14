b512cd8cd8c570bc53406b3b3dc50611f274cd29
ayaabbas286 <ayaabbas286@gmail.com>
2023-12-15 00:02:49 +0200

    a

---
+++ c#/Program.cs
@@ -0,0 +1,47 @@
+﻿/*using comments 
+within a lot of lines*/
+
+// integer variables
+int x = 5;
+int y = 10;
+int sum = x + y;
+Console.WriteLine(sum);
+
+// Using string variables
+string Username = "aya";
+Console.WriteLine("hello, " + Username);
+
+// Using string variables
+char c = 'm';
+Console.WriteLine("my fav character is  " + c);
+
+// using float u must put (f) next to the number 
+float Salary = 3000.789f;
+Console.WriteLine("your Salary is:" + Salary);
+
+//unit for postive value for int only 
+uint a = 5;
+Console.WriteLine(a);
+
+//byte for integer postive / sbyte int for negative value
+byte num1 = 10;
+sbyte num2 = -10;
+Console.WriteLine(num1 + num2);
+
+//bool for true or false value
+bool value1 = true;
+bool value2 = false;
+Console.Write(value2);
+Console.WriteLine(value1);
+
+// object varaiable accep any value
+object greetings = "Welcome to our website";
+Console.WriteLine(greetings);
+
+
+//sizeof(name of variable)
+Console.WriteLine(sizeof(bool));
+
+//constant variable must have the decleration and value within the same line;
+const int H = 15;
+Console.WriteLine(H);
